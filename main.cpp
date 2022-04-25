b5a0e536d909f2909dec2fa28cbf8239eca39440
YoussefOsama200 <youssef.ali001@eng-st.cu.edu.eg>
2022-04-26 01:58:02 +0200

    declare some variables

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+struct Card
+{
+    int type=0;
+    int number=0;
+};
 class CardWarGame{
-
+   Card *deckCards1= new Card[26]; //declar arrays in heap to prevent stack overflow
+   Card *deckCards2= new Card[26];
 };
 int main() {
    cout << "Hello, World!" << endl;
