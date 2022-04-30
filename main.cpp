a7055628e51657f24f53b8dee718f0237e987a17
YoussefOsama200 <youssef.ali001@eng-st.cu.edu.eg>
2022-04-30 08:39:59 +0200

    print function added and ask for war function added , waiting game engine function which will run the print function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
-
+//SPADE   "\x06"
+//CLUB    "\x05"
+//HEART   "\x03"
+//DIAMOND "\x04"
 #define J 11
 #define Q 12
 #define K 13
@@ -92,18 +95,15 @@ class Queue
 
         }
 };
-
-class node
+class Node
 {
     public:
         Card card;
-        node *next;
-
-
-        void listAppend(node **head_ref,Card new_card)
+        Node *next;
+        void listAppend(Node **head_ref, Card new_card)
             {
-                node *tmp = *head_ref;
-                node *new_node = new node();
+                Node *tmp = *head_ref;
+                Node *new_node = new Node();
                 new_node->card = new_card;
                 new_node->next = NULL;
                 if (!tmp)
@@ -122,12 +122,12 @@ class node
                 }
             }
 
-        void deleteList(node** head_ref)
+        void deleteList(Node** head_ref)
             {
 
                 /* deref head_ref to get the real head */
-                node* current = *head_ref;
-                node* next = NULL;
+                Node* current = *head_ref;
+                Node* next = NULL;
 
                 while (current != NULL)
                 {
@@ -138,7 +138,7 @@ class node
                 *head_ref = NULL;
             }
 
-        Card getnth(node *head_ref, int pos)
+        Card getnth(Node *head_ref, int pos)
             {
                 for (int i=1; i < pos; i++)
                 {
@@ -155,74 +155,119 @@ class node
                 return head_ref->card;
             }
 };
-
-
-
-
-
 class CardWarGame{
     //Queues that represent what in players hands
     Queue queue1;
     Queue queue2;
-    // variables that represents the cards that will be used in the war
-   Card *deckCards1= new Card[26]; //declar arrays in heap to prevent stack overflow
-   Card *deckCards2= new Card[26];
+    // variables that represent the cards that will be used in the war
+   Node* deckCards1= new Node;
+   Node* deckCards2= new Node;
     //Variables that represent how many cards to draw for each player in the current card war
-    int CardsToDraw1;
-    int CardsToDraw2;
+    int cardsToBid1[52]={0}; //declare arrays in heap to prevent stack overflow
+    int cardsToBid2[52]={0};
+public:
    CardWarGame(){
    //TODO:: omar ashraf will make a constructor that will prepare two queues that represent cards in each player hands and at the end of the constructor it will call Game Engine function to start the game
    }
    void print(){
-   //TODO:: osama will make this function that depends on two queues
+       cout << "           player 1            VS                player  2" <<
+            endl <<
+            "            " <<
+            queue1.size() <<                     //tell each player how many cards he has in his hands
+            " Cards" <<
+            "                              " <<
+            queue2.size() <<
+            " Cards" <<
+            endl<<"____________________________________________________________________"<<endl;
+       Card card1 =deckCards1->getnth(deckCards1,1), card2=deckCards2->getnth(deckCards2,1);
+       printCards(card1,card2);
+       int currentCardNumber1=1,currentCardNumber2=1;
+       Card flippedCard,emptyCard;
+       flippedCard.number=0;
+       emptyCard.number=-1;
+       for (int i=0;cardsToBid1[i]!=0&&cardsToBid2[i];i++)
+       {
+           for(int j=1;j<=(cardsToBid1[i]>cardsToBid2[i]?cardsToBid1[i]:cardsToBid2[i]);j++) //the loop will iterate until j becomes equal the greater between the number of cards to bid the two players choose
+           {
+               printCards((j<cardsToBid1[i]?flippedCard:(j==cardsToBid1[i]?deckCards1->getnth(deckCards1,currentCardNumber1):emptyCard)),
+                          (j<cardsToBid2[i]?flippedCard:(j==cardsToBid2[i]?deckCards2->getnth(deckCards1,currentCardNumber2):emptyCard)));
+               if(j<=cardsToBid1[i])
+                   currentCardNumber1++;
+               if(j<=cardsToBid2[i])
+                   currentCardNumber2++;
+           }
+       }
+       cout<<"                         comparing between"<<endl;
+       printCards(deckCards1->getnth(deckCards1,currentCardNumber1),deckCards2->getnth(deckCards2,currentCardNumber2));
+   }
+   void printCards(Card card1,Card card2){
+       /*
+        print cards function prints two cards , it can print one faced up card and the other is flipped
+        by giving value 0 to the number of the flipped card, and it can also print one card only by giving -1
+        to the number of the absent card
+        */
+       if (card1.number>0&&card2.number>0) {
+           //print type and number of the card
+           cout << "              " <<
+                (card1.number < 11 ? to_string(card1.number) : (card1.number == J ? "J" : (card1.number == K ? "K"
+                                                                                                             : "Q"))) <<
+                (card1.type == Club ? '\x05' : (card1.type == Spade ? '\x06' : (card1.type == Diamond ? '\x04'
+                                                                                                      : '\x03'))) <<
+                "                                   " <<
+                (card2.number < 11 ? to_string(card2.number) : (card2.number == J ? "J" : (card2.number == K ? "K"
+                                                                                                             : "Q"))) <<
+                (card2.type == Club ? '\x05' : (card2.type == Spade ? '\x06' : (card2.type == Diamond ? '\x04'
+                                                                                                      : '\x03'))) <<
+                endl;
+       } else if (card1.number<=0&&card2.number>0){
+           //print type and number of the card
+           cout << (card1.number==0?"            Flipped                                 ":"                                                    " )<<
+                (card2.number < 11 ? to_string(card2.number) : (card2.number == J ? "J" : (card2.number == K ? "K": "Q"))) <<
+                (card2.type == Club ? '\x05' : (card2.type == Spade ? '\x06' : (card2.type == Diamond ? '\x04': '\x03'))) <<
+                endl;
+       } else if (card1.number>0&&card2.number<=0){
+           //print type and number of the card
+           cout << "              " <<
+                (card1.number < 11 ? to_string(card1.number) : (card1.number == J ? "J" : (card1.number == K ? "K"
+                                                                                                             : "Q"))) <<
+                (card1.type == Club ? '\x05' : (card1.type == Spade ? '\x06' : (card1.type == Diamond ? '\x04': '\x03'))) <<
+                (card2.number==0?"                                 Flipped":" ")<<endl;
+       }
    }
    void askForWar(){
-   //TODO:: osama will ask the two players how many cards they want to draw in this turn and update drawInWar1 and drawInWar2 variables
+   cout<<"!!!this round ends to draw!!! \n \nwe have to play another round"<<endl<<"player 1! \nhow many cards you want to bid in this war?"<<endl;
+   int player1CardsToBid;
+   cin>>player1CardsToBid;
+   while(player1CardsToBid>queue1.size())
+   {cout<<"you don't have enough cards , you have only "<<queue1.size()<<" cards\n";
+   cin>>player1CardsToBid;
+   }
+   cout<<"player 2! \nhow many cards you want to bid in this war?"<<endl;
+   int player2CardsToBid;
+   cin>>player2CardsToBid;
+   while(player2CardsToBid>queue2.size())
+   {
+    cout<<"you don't have enough cards , you have only "<<queue1.size()<<" cards\n";
+    cin>>player2CardsToBid;
+    }
+    //adding the bid of each player in the array cardsToBid
+    int i=0;
+    while(cardsToBid1[i]!=0)i++;
+    cardsToBid1[i]=player1CardsToBid;
+    i=0;
+    while(cardsToBid2[i]!=0)i++;
+    cardsToBid2[i]=player1CardsToBid;
    }
    void GameEngine(){
     //    Card Player1_Draw,Player2_Draw;
     //    Player1_Draw=queue1.dequeue();
     //    Player2_Draw=queue2.dequeue();
-
     //    if(Player1_Draw==Player2_Draw)
     //     askForWar();
    }
 };
 int main() {
-     Card cards[5];
-    // Queue new_queue;
-    // Card current;
+    CardWarGame *cardWarGame =new CardWarGame;
 
-     cards[0]={3,5};
-     cards[1]={2,3};
-     cards[2]={1,7};
-     cards[3]={4,6};
-     cards[4]={3,J};
-
-    // for(int i=0;i<5;i++)
-    //     new_queue.enqueue(cards[i]);
-
-    //   cout<<new_queue.size()<<endl<<endl;
-
-    // for(int i=0;i<5;i++)
-    // {
-    //     current=new_queue.dequeue();
-    //     cout<<current.type<<current.number<<endl;
-    // }
-    
-    node *head=NULL;
-    for(int i=0;i<5;i++)
-    head->listAppend(&head,cards[i]);
-    // node *tmp=head;
-    // for(int i=0;i<5;i++){
-    // Card new_card=tmp->card;
-    // cout<<new_card.type<<"\n";
-    // cout<<new_card.number<<"\n";
-    // tmp=tmp->next;
-    // }
-    head->deleteList(&head);
-    Card test_card=head->card;
-    cout<<test_card.type<<"\n";
-    // cout<<test_card.number<<"\n";
     return 0;
 }
